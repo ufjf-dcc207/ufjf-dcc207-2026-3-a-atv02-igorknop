c7e900e958df6e11f532f009c5b671487ab0f5e5
Igor Knop <igor.knop@ufjf.br>
2026-09-03 22:32:02 -0300

    adiciona componente Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Disciplina from "./Disciplina"
+
 function App() {
   return (
     <>
       <h1>Hello world!</h1>
+      <Disciplina />
+      <Disciplina />
+      <Disciplina />
     </>
   )
 
---
+++ src/Disciplina.tsx
@@ -0,0 +1,8 @@
+export default function Disciplina() {
+    return (
+        <div>
+            <code>DCC207</code>
+            <strong>Laboratório Front End</strong>
+        </div>
+    )
+}
