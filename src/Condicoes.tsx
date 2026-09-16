f84bb0382ba2351a76e58cbac4788a764039b4ef
Igor Knop <igor.knop@ufjf.br>
2026-09-15 22:11:09 -0300

    troca por map

--- src/Condicoes.tsx
+++ src/Condicoes.tsx
@@ -1,11 +1,9 @@
-export default function Condicoes(){
+export default function Condicoes() {
     const valores = ["dormindo", "queimando", "congelado", "paralizado"];
-    const elementosJSX = [];
-    for(let i=0; i< valores.length; i++){
-        const elemento = <span key={valores[i]}>{valores[i]}</span>
-        elementosJSX.push(elemento);
-    }
+
     return (<div className="condicoes">
-        {elementosJSX}
+        {valores.map(
+            v => <span key={v}>{v}</span>
+        )}
     </div>)
 }
